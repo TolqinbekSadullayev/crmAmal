6af56709af49e85f88b8c74ae503cf520665f9e7
Davronbek <davronubaydullayev8778@gmail.com>
2023-04-27 16:38:55 +0500

    vazifa

--- src/Components/Xonalar/Xonalar.jsx
+++ src/Components/Xonalar/Xonalar.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Xonalar() {
   return (
-    <div>Xonalar</div>
+    <div>
+      <div className="row d-flex">
+         <div  className="col-3">
+               <h2>Xonalar</h2>
+         </div>
+         <div className="col-3">
+           <button  className='btn btn-success'>Xona qoshish</button>
+          
+         </div>
+      </div>
+
+    </div>
   )
 }
