63b50628a5accd4402dd791ecc811b53a4fc16ea
TolqinbekSadullayev <9527125st@gmail.com>
2023-05-01 15:58:07 +0500

    register page yangilandi va about qism qo'shildi

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,74 @@
 import React from 'react'
+import './About.css'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+      <div class="container1">
+    <div className="row">
+    <div class="card11 col-3 my-3 card0">
+      <div class="borderr">
+        <h2 id='h2'>To'lqinbek Sadullayev</h2>
+        <div class="icons">
+          <i class="fa fa-youtube" aria-hidden="true"></i>
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+          <i class="fa fa-dribbble" aria-hidden="true"></i>
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+        </div>
+      </div>
+    </div>
+    <div class="card11 col-3 my-3 card1">
+      <div class="borderr">
+        <h2 id='h2'>Farangiz Ismatova</h2>
+        <div class="icons">
+          <i class="fa fa-youtube" aria-hidden="true"></i>
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+          <i class="fa fa-dribbble" aria-hidden="true"></i>
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+        </div>
+      </div>
+    </div>
+    <div class="card11 col-3 my-3 card2">
+      <div class="borderr">
+        <h2 id='h2'>Muhammadazim</h2>
+        <div class="icons">
+          <i class="fa fa-youtube" aria-hidden="true"></i>
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+          <i class="fa fa-dribbble" aria-hidden="true"></i>
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+        </div>
+      </div>
+    </div>
+    <div class="card11 col-3 my-3 card2">
+      <div class="borderr">
+        <h2 id='h2'>Davron</h2>
+        <div class="icons">
+          <i class="fa fa-youtube" aria-hidden="true"></i>
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+          <i class="fa fa-dribbble" aria-hidden="true"></i>
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+        </div>
+      </div>
+    </div>
+    <div class="card11 col-3 my-3 card2">
+      <div class="borderr">
+        <h2 id='h2'>Ibrat</h2>
+        <div class="icons">
+          <i class="fa fa-youtube" aria-hidden="true"></i>
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+          <i class="fa fa-dribbble" aria-hidden="true"></i>
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+
+    </div>
   )
 }
