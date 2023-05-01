cfa9b2726dee279d36b8f5956d0f899135390463
TolqinbekSadullayev <9527125st@gmail.com>
2023-05-01 16:58:57 +0500

    go

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -10,11 +10,11 @@ export default function About() {
       <div class="borderr">
         <h2 id='h2'>To'lqinbek Sadullayev</h2>
         <div class="icons">
-          <i class="fa fa-youtube" aria-hidden="true"></i>
-          <i class="fa fa-instagram" aria-hidden="true"></i>
-          <i class="fa fa-dribbble" aria-hidden="true"></i>
-          <i class="fa fa-twitter" aria-hidden="true"></i>
-          <i class="fa fa-facebook" aria-hidden="true"></i>
+          <a href="https://www.youtube.com/tolqinbeksadullayev2045/" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+          <a href="https://www.instagram.com/tolqin_sadullayev/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+          <a href="https://t.me/tolqin_sadullayev/" target="_blank"><i class="fa fa-telegram" aria-hidden="true"></i></a>
+          <a href="https://www.twitter.com/SadullayevTo/" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+          <a href="https://www.facebook.com/tolqinbek.sadullayev.25/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
         </div>
       </div>
     </div>
@@ -22,11 +22,11 @@ export default function About() {
       <div class="borderr">
         <h2 id='h2'>Farangiz Ismatova</h2>
         <div class="icons">
-          <i class="fa fa-youtube" aria-hidden="true"></i>
-          <i class="fa fa-instagram" aria-hidden="true"></i>
-          <i class="fa fa-dribbble" aria-hidden="true"></i>
-          <i class="fa fa-twitter" aria-hidden="true"></i>
-          <i class="fa fa-facebook" aria-hidden="true"></i>
+        <a href="" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+          <a href="" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+          <a href="" target="_blank"><i class="fa fa-telegram" aria-hidden="true"></i></a>
+          <a href="" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+          <a href="" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
         </div>
       </div>
     </div>
@@ -34,11 +34,11 @@ export default function About() {
       <div class="borderr">
         <h2 id='h2'>Muhammadkarimov Abduazim</h2>
         <div class="icons">
-          <i class="fa fa-youtube" aria-hidden="true"></i>
-          <i class="fa fa-instagram" aria-hidden="true"></i>
-          <i class="fa fa-dribbble" aria-hidden="true"></i>
-          <i class="fa fa-twitter" aria-hidden="true"></i>
-          <i class="fa fa-facebook" aria-hidden="true"></i>
+        <a href="" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+          <a href="" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+          <a href="" target="_blank"><i class="fa fa-telegram" aria-hidden="true"></i></a>
+          <a href="" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+          <a href="" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
         </div>
       </div>
     </div>
@@ -46,11 +46,11 @@ export default function About() {
       <div class="borderr">
         <h2 id='h2'>Davron</h2>
         <div class="icons">
-          <i class="fa fa-youtube" aria-hidden="true"></i>
-          <i class="fa fa-instagram" aria-hidden="true"></i>
-          <i class="fa fa-dribbble" aria-hidden="true"></i>
-          <i class="fa fa-twitter" aria-hidden="true"></i>
-          <i class="fa fa-facebook" aria-hidden="true"></i>
+        <a href="" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+          <a href="" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+          <a href="" target="_blank"><i class="fa fa-telegram" aria-hidden="true"></i></a>
+          <a href="" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+          <a href="" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
         </div>
       </div>
     </div>
@@ -58,11 +58,11 @@ export default function About() {
       <div class="borderr">
         <h2 id='h2'>Ibrat</h2>
         <div class="icons">
-          <i class="fa fa-youtube" aria-hidden="true"></i>
-          <i class="fa fa-instagram" aria-hidden="true"></i>
-          <i class="fa fa-dribbble" aria-hidden="true"></i>
-          <i class="fa fa-twitter" aria-hidden="true"></i>
-          <i class="fa fa-facebook" aria-hidden="true"></i>
+        <a href="" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+          <a href="" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+          <a href="" target="_blank"><i class="fa fa-telegram" aria-hidden="true"></i></a>
+          <a href="" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+          <a href="" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
         </div>
       </div>
     </div>
